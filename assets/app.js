09f3a5e831146da5fb18f3c530e725645651217f
Devin DiNapoli <devindinapoli@gmail.com>
2018-03-07 17:45:24 -0500

    added js functions

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     var initialButtons = ["Rick & Morty", "The Simpsons", "Shameless", "Black Mirror"];
 
     function displayButtons(){
-        $("#display-gifs").empty();
 
         for(i = 0; i < initialButtons.length; i++){
             var newButton = $("<button>");
@@ -17,8 +16,39 @@ $(document).ready(function(){
 
     displayButtons();
 
+    function showGifs(){
+       $ ("#display-gifs").empty();
+       var searchQuery = $(this).attr("data-name");
+       var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=IVQQNCCTzubcYVQLjKR29Pc3ed1j7k22&q=" + 
+       searchQuery + "&limit=10&lang=en";
 
+       $.ajax({
+           url: queryURL,
+           method: "GET"
+       }).then(function(response){
 
+            for(j = 0; j < 10; j++){
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+
+                var gif = $("<img>");
+                gif.attr("src", response.data[j].images.original_still.url);
+                gif.attr("data-still", response.data[j].images.original_still.url);
+                gif.attr("data-animate", response.data[j].images.original.url);
+                gif.attr("data-state", "still");
+                gif.attr("class", "gif");
+                displayDiv.append(gif);
+
+                var rating = response.data[j].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                displayDiv.append(p);
+
+                $("#display-gifs").append(displayDiv);
+            }
+       });
+    }
+
+    
 
 
 
